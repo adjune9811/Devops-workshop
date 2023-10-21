806476af507ef9ff4619b0bac3485ba1d8b58a36
Ajay <adjune9811@gmail.com>
2023-10-21 18:58:02 +0530

    adding v2 Version

---
+++ terraform_code/V2-EC2.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-0df435f331839b2d6"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+  security_groups = ["demo-sg"]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+//  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+tags =   {
+ Name = "ssh-port" 
+  }
+
+  }
